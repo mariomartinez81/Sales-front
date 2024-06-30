11f62febfe99a5f7c8aa4e2bb5b7564801ec3286
“Mario <mmartinez@blossom.net>
2024-06-29 22:12:51 -0500

    feat: 🎉 start frontend project

---
+++ src/app/ClientLayout.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import queryClientContext from '../context/queryClientContext';
+
+interface ClientLayoutProps {
+  children: React.ReactNode;
+}
+
+const ClientLayout = ({ children }: ClientLayoutProps) => {
+  return (
+    <QueryClientProvider client={queryClientContext}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default ClientLayout;
